3429323cd445f36f352e85c2b7bbb7f6f555eaf1
tlaze <tomlazore@gmail.com>
2021-11-22 22:55:47 -0500

    Added component files for each section

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import About from './components/About';
+import Navbar from './components/Navbar';
+import Menu from './components/Menu';
+import Events from './components/Events';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <About/>
+      <Navbar />
+      <About />
+      <Menu />
+      <Events />
+      <Contact />
+      <Footer />
     </div>
   );
 }
