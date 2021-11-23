8e681a7f3c033d49f743d81db2670a97aafdb42e
tlaze <tomlazore@gmail.com>
2021-11-23 11:40:10 -0500

    Updated header image and About text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,27 @@
-function About() {
+import React from 'react';
+import AboutPic from '../assets/images/AboutPic.jpg'
 
+
+function About() {
+    const styles = {
+        headerPic: {
+            height: '500px',
+            backgroundImage: `url(${AboutPic})`,
+            backgroundPosition: 'center',
+        },
+        headerText: {
+            color:'white',
+            textAlign:'center',
+            lineHeight: '450px',
+            fontSize: '75px',
+        }
+      }
     return(
-        <h1>About Section</h1>
+        <div>
+            <header style={styles.headerPic}>
+                <h1 style={styles.headerText}>About</h1>
+            </header>
+        </div>
     )
 
 }
