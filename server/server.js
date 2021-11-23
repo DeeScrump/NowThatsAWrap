ef9a110d81e0f3c61b0dc77f0153caa8dfd1d366
bindi-v <bindi.vaghela@gmail.com>
2021-11-23 12:03:39 -0500

    added necessary dependencies and config/connection for server, server is running good!

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 //const { typeDefs, resolvers } = require('./schemas');
 //const { authMiddleware } = require('./utils/auth');
 
@@ -29,11 +29,11 @@ if (process.env.NODE_ENV === 'production') {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`API server running on http://localhost:${PORT}`);
    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
