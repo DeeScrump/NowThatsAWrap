6b38d63d398e791e6568cc8aed91c77a610453d6
Tammy Gagliano <tracsd@gmail.com>
2021-11-24 13:36:40 -0600

    update menu to add menu items

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,33 +1,31 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import BreakfastQuesadillas from "../pages/breakfast/BreakfastQuesadillas";
-import BreakfastWraps from "../pages/breakfast/BreakfastWraps";
-import DeluxeOmelets from "../pages/breakfast/DeluxeOmelets";
-import ChickenWraps from "../pages/lunch/ChickenWraps";
-import SeafoodWraps from "../pages/lunch/SeafoodWraps";
-import VeganWraps from "../pages/lunch/VeganWraps";
+import { BreakfastQuesadillas } from "../pages/breakfast/BreakfastQuesadillas";
+// import BreakfastWraps from "../pages/breakfast/BreakfastWraps";
+// import DeluxeOmelets from "../pages/breakfast/DeluxeOmelets";
+// import ChickenWraps from "../pages/lunch/ChickenWraps";
+// import SeafoodWraps from "../pages/lunch/SeafoodWraps";
+// import VeganWraps from "../pages/lunch/VeganWraps";
 
-function Menu({ items }) {
+function Menu() {
+  console.log(BreakfastQuesadillas);
   return (
     <div className="container">
       <h1>BREAKFAST</h1>
-        <p>{BreakfastQuesadillas[0]}</p>
-      {/* <h1>Menu Page</h1>
-        <div className="section-center">
-      {item.map((item) => {
-        const { id, title, img, desc, price } = item;
+      {BreakfastQuesadillas.map(({id, title, img, price, desc}) => (
+        <article key={id} className="menu-item">
+          <img src={img} alt={title} className="photo" />
+          <div className="item-info">
+            <header>
+              <h4>{title}</h4>
+              <h4 className="price">${price}</h4>
+            </header>
+            <p className="item-text">{desc}</p>
+          </div>
+        </article>
+      ))}
 
-          <article key={id} className="menu-item">
-            <img src={img} alt={title} className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">${price}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
-            </div>
-          </article>
-      }}} */}
+      <div className="section-center"></div>
     </div>
   );
 }
