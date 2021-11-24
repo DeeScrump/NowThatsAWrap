b3b9ea0a95d9da1a5287850e3d6fe565541a9258
Tammy Gagliano <tracsd@gmail.com>
2021-11-24 10:17:58 -0600

    add menu items to pages

---
+++ client/src/pages/lunch/SeafoodWraps.js
@@ -0,0 +1,44 @@
+const seafoodWraps = [
+    {
+      id: 1,
+      title: "Caesar Seafood",
+      category: "lunch",
+      price: 15.00,
+      img: "/client/public/assets/images/IMG-20211122-WA0035.jpg",
+      desc: `Jumbo Shrimp & Imitation Crab Meta, Lettuce, Grated Parmesan Cheese and Caesar Dressing.`,
+    },
+    {
+      id: 2,
+      title: "Grilled Jumbo Shrimp",
+      category: "lunch",
+      price: 15.00,
+      img: "/client/public/assets/images/IMG-20211122-WA0035.jpg",
+      desc: `Grilled Jumbo Shrimp, Lettuce, Grated Parmesan Cheese and Chipotle Ranch`,
+    },
+    {
+      id: 3,
+      title: "Grilled Salmon Burger",
+      category: "lunch",
+      price: 15.00,
+      img: "/client/public/assets/images/IMG-20211122-WA0035.jpg",
+      desc: `Grilled Salmon Burger, Lettuce, Grated Parmesan Cheese and Chipotle Ranch`,
+    },
+    {
+      id: 4,
+      title: "Philly Seafood",
+      category: "lunch",
+      price: 20.00,
+      img: "/client/public/assets/images/IMG-20211122-WA0035.jpg",
+      desc: `Jumbo Shrimp & Imitation Crab Meat, Sauteed Bell Peppers, Mushrooms, Onions, Mozzarella Cheese and Chipotle Ranch`,
+    },
+    {
+      id: 5,
+      title: "Surf & Turf",
+      category: "lunch",
+      price: 20.00,
+      img: "/client/public/assets/images/IMG-20211122-WA0035.jpg",
+      desc: `USDA Choice Steak, Grilled Jumbo Shrimp, Lettuce, Grated Parmesan Cheese and Chipotle Ranch`,
+    }
+  ];
+  export default seafoodWraps;
+  
